3a0d265c92b13ff35dd988cb843eb6df4b55dde5
hey <44032576+YASSIN974@users.noreply.github.com>
2019-01-14 18:56:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -859,5 +859,22 @@ client.on("message", message => {
 		} 
 	} 
 });
+var AsciiTable = require('ascii-data-table').default
+client.on('message', message =>{
+
+    if(message.content == "#roles"){
+        if(message.guild.member(message.author).hasPermission("ADMINISTRATOR"))
+        var 
+        ros=message.guild.roles.size,
+        data = [['Rank', 'RoleName']]
+        for(let i =0;i<ros;i++){
+            if(message.guild.roles.array()[i].id !== message.guild.id){
+         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
+        }}
+        let res = AsciiTable.table(data)
+
+        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
+    }
+});
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
