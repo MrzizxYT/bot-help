8987a26ad231cc398df38ced7aed92dab8a71aad
hey <44032576+YASSIN974@users.noreply.github.com>
2019-01-15 22:49:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1487,88 +1487,41 @@ const channel = sWlc[message.guild.id].channel
   }
 });
 client.on('guildMemberAdd', member => {
-let channel = member.guild.channels.get("528306102815948820");
-if (member.user.bot) return;
-var Canvas = require('canvas')
-var jimp = require('jimp')
-  var currentTime = new Date(),
-hours = currentTime.getHours() + 3 ,
-minutes = currentTime.getMinutes(),
-seconds = currentTime.getSeconds();
-if (minutes < 10) {
-minutes = "0" + minutes;
-}
-var suffix = "AM";
-if (hours >= 12) {
-suffix = "PM";
-hours = hours - 12;
-}
-if (hours == 0) {
-hours = 12;
-}
-const w = ['./w1png'];
-             let Image = Canvas.Image,
-                  canvas = new Canvas(557, 241),
-                  ctx = canvas.getContext('2d');
-              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-                  if (err) return console.log(err)
-                  let BG = Canvas.Image;
-                  let ground = new Image;
-                  ground.src = Background;
-                  ctx.drawImage(ground, 0, 0, 557, 241);
-
-             
-
-      })
-                      let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
-                      jimp.read(url, (err, ava) => {
-                          if (err) return console.log(err);
-                          ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-                              if (err) return console.log(err);
-
-                                    ctx.font = '30px Arial Bold';
-                              ctx.fontSize = '20px';
-                              ctx.fillStyle = "#FFFFFF";
-                                ctx.fillText(member.user.username, 277, 138);
-
-                                                     var guild;
-    while (!guild)
-                        guild = member.guild
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
-                        member.guild.fetchInvites().then(invs => {
-      let user = Invite.inviter;
-      let invites = invs.filter(i => i.inviter.id === user.id);
-      let inviteCount = invites.reduce((p, v) => v.uses + p, 0);
-
- channel.send(`**Invited By** : **${Invite.inviter}**`);
-
-})
-}
-            dat[Inv] = Invite.uses;
-})
-})
-
-                              //AVATARً
-                              let Avatar = Canvas.Image;
-                              let ava = new Avatar;
-                              ava.src = buf;
-                              ctx.beginPath();
-                 ctx.arc(120.8, 120.5, 112.3, 0, Math.PI*2, true);
-                   ctx.closePath();
-
-                                 ctx.clip();
-
-                        ctx.drawImage(ava, 7, 8, 227, 225);
-                              ctx.closePath();
- channel.sendFile(canvas.toBuffer())
-                          })
-})
+    var embed = new Discord.RichEmbed()
+    .setThumbnail(member.user.avatarURL)
+  .addField("**:rose: THANKS FOR JOINING OUR SERVER:rose: :" ,member.user.username )
+    .setDescription(':heart: HOPE YOU ENJOY IN FALLEGA TEAM ᴳᴬᴹᴵᴺᴳ SERVER:heart: : *')
+    .setColor('RANDOM')
+    .setImage('https://cdn.pg.sa/OD53Wjr9p0.gif%27)
+var channel =member.guild.channels.find('name', '【welcome】')
+if (!channel) return;
+channel.send({embed : embed});
 });
+
+
+client.on("guildMemberAdd", member => {
+    member.createDM().then(function (channel) {
+    return channel.send(:rose: MAR7BABIK FI SERVER FALLEGA TEAMM :rose: 
+  :crown: MAR7BA SI  ${member}:crown:  
+  :arrow_right:  YOU ARE THE MEMBER NUMBER :  ${member.guild.memberCount}) 
+  }).catch(console.error)
+});
+
+
+client.on('guildMemberRemove', member => {
+    var embed = new Discord.RichEmbed()
+    .setAuthor(member.user.username, member.user.avatarURL)
+    .setThumbnail(member.user.avatarURL)
+    .setTitle(Cya ✋:skin-tone-1: 😔)
+    .setDescription(Goood bye  ✋:skin-tone-1: 😔)
+    .addField('in the server :bust_in_silhouette::',`[ ${member.guild.memberCount} ]**,true)
+    .setColor('RED')
+    .setFooter(==== (っ◔◡◔)っ :hearts: 𝐇𝐎𝐏𝐄 𝐘𝐎𝐔 𝐄𝐍𝐉𝐎𝐘 :hearts: ====`, 'https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png%27)
+
+var channel =member.guild.channels.find('name', '【welcome】')
+if (!channel) return;
+channel.send({embed : embed});
+})
       
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
