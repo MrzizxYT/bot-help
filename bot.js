6015027afbbcc99244ca222389b2859adaf8af65
hey <44032576+YASSIN974@users.noreply.github.com>
2019-01-15 23:03:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1487,4 +1487,24 @@ const channel = sWlc[message.guild.id].channel
   }
 });
 
+client.on("guildMemberAdd", member => {
+let welcomer = member.guild.channels.find("name","welcome");
+      if(!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var h = member.user;
+        let norelden = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(h.avatarURL)
+        .setAuthor(h.username,h.avatarURL)
+        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)
+        .addField(': انت عضو رقم',`${member.guild.memberCount}`,true)
+         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+     welcomer.send({embed:norelden});          
+               
+ 
+      }
+      });
+
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
