184a3021814d564860db27a7ef3aa5de8e97752e
hey <44032576+YASSIN974@users.noreply.github.com>
2019-02-02 12:56:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1411,41 +1411,8 @@ client.on('guildCreate', guild => {
     .setDescription(`**شكراً لك لإضافه البوت الى سيرفرك**`)
         guild.owner.send(embed)
   });
-client.on('guildCreate', guild => {
+
    
-    client.users.get("332713449215754242")
-      .setAuthor(`بوتك دخل سيرفر جديد مبروك ✅`)
-      .setDescription(`**
-    Server name: __${guild.name}__
-    Server id: __${guild.id}__
-    Server owner: __${guild.owner}__
-    Member Count: __${guild.memberCount}__
-    Servers Counter : __${client.guilds.size}__**`)
-            .setColor('#2019e')
-            .setFooter('!Relax' , client.user.avatarURL)
-           client.users.get("ا332713449215754242").send({Dark&Narox});
-  })
-   
-  client.on('guildDelete', guild => {
-     
-    client.users.get("332713449215754242")
-    .setAuthor(`Fenixo system. left a server ❎`)
-    .setDescription(`**
-   Server name: __${guild.name}__
-   Server id: __${guild.id}__
-   Server owner: __${guild.owner}__
-   Members Count: __${guild.memberCount}__
-   Servers Counter : __${client.guilds.size}__**`)
-           .setColor('##2019e')
-           .setFooter('!Relax' , client.user.avatarURL)
-           client.users.get("332713449215754242").send({Dark&Narox});
-   
-  });
-
-
-
-
-
 
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
